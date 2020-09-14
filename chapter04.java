a56bef2843510021142c2ec846124e19a4c39678
komiyusuke <a53@53.lan>
2020-09-14 14:53:46 +0900

    chapter4

---
+++ chapter04.java
@@ -0,0 +1,11 @@
+
+public class chapter04 {
+	 public static void main(String[] args) {
+	        final int num1 = 5;
+	        String str1 = "abcd";
+	        System.out.println(num1);
+	        System.out.println(str1);
+	        str1 = "efg";
+	        System.out.println(str1);
+	    }
+}
